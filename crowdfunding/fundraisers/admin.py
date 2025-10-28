ad7bb507764d695fcee61f1a65b46e7c439e7cb1
jin <jf.lily.shan@gmail.com>
2025-10-28 20:01:25 +0800

    admin view

--- crowdfunding/fundraisers/admin.py
+++ crowdfunding/fundraisers/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Campaign, Fundraiser, Pledge, Sponsor
+
+admin.site.register(Campaign)
+admin.site.register(Fundraiser)
+admin.site.register(Pledge)
+admin.site.register(Sponsor)
